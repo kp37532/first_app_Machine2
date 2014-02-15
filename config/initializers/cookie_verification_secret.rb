ebd77a11658a632ad7ea5c8c5d31d678ffecb8e9
kp37532 <pinto.orville@gmail.com>
2014-02-14 20:58:31 -0500

    Initialize repository

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '72ec74853015a2858e3ede681fb24dad2eb236292be219e7c179b679befc76a9fc4c5fecb92de4131ef69e02cd35e4aeb45724ee7d955790a2ef4c9a9785a86f';
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '4f4f21d532a1bfa315b39d662794144323201f90d26a7eb8090cfd5afa03b63f267f6977af619c23f16dc0e4c03dafd9dcfc58219e531ba1ed644c46bb3fa445'
